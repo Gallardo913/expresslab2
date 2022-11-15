cf9c47014ec0f7abb23c18a55558e4e39c432873
Roger gallardo <rogergallardofs1@gmail.com>
2022-11-15 01:35:05 -0500

    commit 3

--- server.js
+++ server.js
@@ -21,4 +21,15 @@ app.get('/tip', function (req, res) {
       res.send(`${parseInt(req.params.total) * (parseInt(req.params.tip)) / 100}`)
     })
 
+app.get('/magic', function(req, res) {
+        res.send('Question')
+      })
+      
+    app.get('/magic/:question', function(req, res) {
+        res.send((req.params.question) + " " + eightBallResponse[Math.floor(Math.random()*eightBallResponse.length)])
+     })
+      
+ const eightBallResponse = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely","You may rely on it", "As I see it yes", "Most likely", "Outlook good","Yes", "Signs point to yes", "Reply hazy try again", "Ask again later","Better not tell you now", "Cannot predict now", "Concentrate and ask again","Don't count on it", "My reply is no", "My sources say no","Outlook not so good", "Very doubtful"]
+      
+
 app.listen(3000)
